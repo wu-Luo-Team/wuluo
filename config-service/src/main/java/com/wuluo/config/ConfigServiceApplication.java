c9796af9189f9be09bd508e3631480729e131dcf
sacher <zechun.wang@ifchange.com>
2019-12-27 18:18:24 +0800

    add config

---
+++ config-service/src/main/java/com/wuluo/config/ConfigServiceApplication.java
@@ -0,0 +1,20 @@
+package com.wuluo.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Description：
+ * @Author：sacher
+ * @Create：2019/12/27 6:02 下午
+ **/
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
--- modules/user-service-parent/user-service/src/main/java/com/wuluo/user/UserServiceApplication.java
+++ modules/user-service-parent/user-service/src/main/java/com/wuluo/user/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.wuluo.user;
 import com.wuluo.common.core.constant.CommonConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
